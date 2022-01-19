08bf2c7b117d4648435df12c0034c78c3ad038b6
jbson_x240 <jb@jbson.net>
2022-01-19 14:30:49 +0100

    test add block to chain & get current block index

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -9,70 +9,19 @@ class TestBlockchain(unittest.TestCase):
     _blockchain: Blockchain
 
     def test_blockchain_creation(self):
+        """create an instance of blockchain with a genesis block"""
         blockchain = Blockchain()
         self.assertEqual(blockchain.length(), 1)
 
     def test_blockchain_add_block(self):
+        """"""
         self._blockchain = Blockchain()
-        a_random_block = Block(
-            index=0, timestamp=time.time(), previous_hash="GENESIS")
-        self.assertEqual(self._blockchain.length(), 1)
-        self.assertNotEqual(self._blockchain.current_block.hash,
-                            a_random_block.hash)
+        added_block = self._blockchain.new_block(previous_hash="GENESIS")
+        self.assertNotEqual(added_block, None)
 
-    def test_blockchain_get_last_block(self):
-        pass
-
-    def test_blockchain_get_last_block_index(self):
-        pass
-
-    def test_blockchain_get_last_block_hash(self):
-        pass
-
-    def test_blockchain_get_last_block_timestamp(self):
-        pass
-
-    def test_blockchain_get_last_block_previous_hash(self):
-        pass
-
-    def test_blockchain_get_last_block_proof(self):
-        pass
-
-    def test_blockchain_get_last_block_records(self):
-        pass
-
-    def test_blockchain_get_last_block_record(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_index(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_hash(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_index(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_hash(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_timestamp(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_previous_hash(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_proof(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_sender(self):
-        pass
-
-    def test_blockchain_get_last_block_record_by_type_and_recipient(self):
-        pass
+    def test_blockchain_current_block(self):
+        self.test_blockchain_add_block()
+        self.assertEqual(self._blockchain.current_block.index, 1)
 
 
 if __name__ == '__main__':
